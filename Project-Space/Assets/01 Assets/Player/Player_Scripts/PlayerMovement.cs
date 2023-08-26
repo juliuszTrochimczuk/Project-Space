6141c84743037933bd1f6492c06ea819118f0591
Juliusz Trochimczuk <assasindal@gmail.com>
2023-08-26 17:59:45 +0000

    Feature / Magic Pen Pool

--- Project-Space/Assets/01 Assets/Player/Player_Scripts/PlayerMovement.cs
+++ Project-Space/Assets/01 Assets/Player/Player_Scripts/PlayerMovement.cs
@@ -66,7 +66,7 @@ namespace Player
         {
             transform.Rotate(new Vector3(0.0f, mouseRotation.x, 0.0f) * rotationSpeed * Time.deltaTime);
             camYRotation += -mouseRotation.y * rotationSpeed * Time.deltaTime;
-            camYRotation = Mathf.Clamp(camYRotation, -65f, 65f);
+            camYRotation = Mathf.Clamp(camYRotation, -80f, 80f);
             mainCam.transform.eulerAngles = new Vector3(camYRotation, transform.eulerAngles.y, 0.0f);
         }
 
